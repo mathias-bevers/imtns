b851ce497986ca356c1f8e4a821a33ea24193064
Mathias Bevers <mathiasbevers@protonmail.com>
2026-02-23 14:12:58 +0100

    created menu en menu manager
    - menu is an abstract behavior, it mainly serves the perpose to be
    opened/closed throught the menu manager
    - menu manager is a singleton that keeps track of all the menus in the
    scene and opens/closes them based on types.

--- Assets/1. Code/Menus/Menu.cs
+++ Assets/1. Code/Menus/Menu.cs
@@ -11,5 +11,74 @@ namespace CleanRoom.Menus
 
         public event Action openedEvent;
         public event Action closedEvent;
+
+        private GameObject content;
+        protected GameObject Content
+        {
+            get
+            {
+                if (!ReferenceEquals(null, content))
+                {
+                    return content;
+                }
+
+                if (transform.childCount != 1)
+                {
+                    throw new NotSupportedException(
+                        $"The only child of {GetType().Name} should be the \"Content\" GameObject.");
+                }
+
+                content = transform.GetChild(0).gameObject;
+                return content;
+            }
+        }
+
+        protected void Start()
+        {
+            if (IsHUD)
+            {
+                Open();
+            }
+            else
+            {
+                Close();
+            }
+
+            MenuManager.Instance.RegisterMenu(this);
+        }
+
+        public void Open()
+        {
+            if (!CanBeOpened())
+            {
+                Debug.LogWarning($"{GetType().Name} does not meet the opening criteria");
+                return;
+            }
+            
+            Content.SetActive(true);
+            IsOpen = true;
+            openedEvent?.Invoke();
+        }
+
+        public void Close()
+        {
+            if (!CanBeClosed())
+            {
+                Debug.LogWarning($"{GetType().Name} does not meet the closing criteria");
+                return;
+            }
+            
+            Content.SetActive(false);
+            IsOpen = false;
+            closedEvent?.Invoke();
+        }
+
+        protected abstract bool CanBeOpened();
+        protected abstract bool CanBeClosed();
+
+        private void OnDestroy()
+        {
+            MenuManager.Instance.UnregisterMenu(this);
+        }
     }
 }
---
+++ Assets/1. Code/Menus/MenuManager.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CleanRoom.Menus
+{
+    public class MenuManager : Singleton<MenuManager>
+    {
+        private readonly HashSet<Menu> menus = new();
+
+        public void RegisterMenu(Menu menu)
+        {
+            menus.Add(menu);
+        }
+
+        public void UnregisterMenu(Menu menu)
+        {
+            if (!menus.Contains(menu))
+            {
+                return;
+            }
+
+            menus.Remove(menu);
+        }
+
+        public T OpenMenu<T>() where T : Menu
+        {
+            T[] possibleToOpen = GetMenusOfType<T>();
+            if (possibleToOpen.Length < 1)
+            {
+                return null;
+            }
+
+            for (int i = 0; i < possibleToOpen.Length; ++i)
+            {
+                T menu = possibleToOpen[i];
+
+                if (menu.IsOpen)
+                {
+                    continue;
+                }
+
+                menu.Open();
+                return menu;
+            }
+
+            return null;
+        }
+
+        public T CloseMenu<T>() where T : Menu
+        {
+            T[] possibleToClose = GetMenusOfType<T>();
+            if (possibleToClose.Length < 1)
+            {
+                return null;
+            }
+
+            for (int i = 0; i < possibleToClose.Length; ++i)
+            {
+                T menu = possibleToClose[i];
+                if (!menu.IsOpen)
+                {
+                    continue;
+                }
+
+                menu.Close();
+                return menu;
+            }
+
+            return null;
+        }
+
+        private T[] GetMenusOfType<T>() where T : Menu => menus.OfType<T>().ToArray();
+        private T GetMenuOfType<T>() where T : Menu => menus.FirstOrDefault(menu => menu is T) as T;
+    }
+}
