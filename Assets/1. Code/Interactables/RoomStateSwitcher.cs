29c5c47269c73715767b42ba0afba058da6bbe95
Mathias Bevers <mathiasbevers@protonmail.com>
2026-05-27 16:34:08 +0200

    added event for when the room is not ready to be left
    - if the player has still minigames to complete, the message bar will
    show up and notify the player about this.
    - all message bars are clickable to get them away.

--- Assets/1. Code/Interactables/RoomStateSwitcher.cs
+++ Assets/1. Code/Interactables/RoomStateSwitcher.cs
@@ -3,6 +3,7 @@ using CleanRoom.Menus;
 using CleanRoom.StateMachine;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace CleanRoom.Interactables
 {
@@ -11,8 +12,11 @@ namespace CleanRoom.Interactables
         private const string ACCENT_HEX = "<color=#A3F2CE>";
         private const int MAX_STARS = 3;
 
+        [SerializeField] private UnityEvent notReadyEvent;
+        
         private PopupMenu popupMenu = null;
         private StateMachine.StateMachine stateMachine = null;
+        
 
         public void RequestNextRoom()
         {
@@ -21,8 +25,7 @@ namespace CleanRoom.Interactables
 
             if (!stateMachine.IsStateCompleted(stateMachine.ActiveState.StateName))
             {
-                popupMenu.CreatePopup("Je hebt nog niet alle minigames in deze kamer voltooid.",
-                    Popup.MessageType.Incorrect, "Nog niet klaar!");
+                notReadyEvent?.Invoke();
                 return;
             }
 
