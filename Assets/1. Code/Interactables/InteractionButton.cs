9fcdb489fdaa2c6be68872f83e77caf17b631b7c
Mathias Bevers <mathiasbevers@protonmail.com>
2026-05-28 14:37:11 +0200

    the interactable button is only active if there is something to interact with
    - the interact button is only active if the player is in an interaction
    zone.
    - fixed a calling order of overlapping interaction zones.

--- Assets/1. Code/Interactables/InteractionButton.cs
+++ Assets/1. Code/Interactables/InteractionButton.cs
@@ -6,16 +6,30 @@ namespace CleanRoom.Interactables
     {
         private InteractionZone interactionZone;
 
+        protected override void Awake()
+        {
+            base.Awake();
+            interactable = false;
+        }
+
         public void OnInteractionZoneEnter(InteractionZone interactionZone)
         {
             this.interactionZone = interactionZone;
             onClick.AddListener(this.interactionZone.OnInteract.Invoke);
+            interactable = true;
         }
 
         public void OnInteractionZoneExit(InteractionZone interactionZone)
         {
-            this.interactionZone = interactionZone;
-            onClick.RemoveListener(this.interactionZone.OnInteract.Invoke);
+            onClick.RemoveListener(interactionZone.OnInteract.Invoke);
+
+            if (!ReferenceEquals(interactionZone, this.interactionZone))
+            {
+                return;
+            }
+
+            interactable = false;
+            this.interactionZone = null;
         }
     }
 }
