4399aa7271149e8685db422521b89a1aca4d40b6
Leroy Exel <Yykgames1283@gmail.com>
2026-06-02 13:59:36 +0200

    Outline shaders & materials added

---
+++ Assets/1. Code/UserInterface/InteractableOutline.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemOutline : MonoBehaviour
+{
+    private Material material;
+
+    [Header("Outline Settings")]
+    [SerializeField] private float maxThickness = 5f;
+    [SerializeField] private float minThickness = 0f;
+    [SerializeField] private float blinkSpeed = 4f;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        float sinValue = Mathf.Sin(Time.time * blinkSpeed);
+
+        float normalizedValue = (sinValue + 1f) / 2f;
+
+        float currentThickness = Mathf.Lerp(minThickness, maxThickness, normalizedValue);
+
+        material.SetFloat("_OutlineThickness", currentThickness);
+    }
+}
