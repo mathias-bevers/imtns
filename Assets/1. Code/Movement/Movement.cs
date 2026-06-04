fd90e179971d33ec54e4f1caeaf5c2bfc42ea890
Mathias Bevers <mathiasbevers@protonmail.com>
2026-06-04 16:28:48 +0200

    player position is saved during transition
    - added a system for loading and saving files
    - created a wrapper struct for vector2 since unity's doesn't play nice
    with newtonsoft
    - saved the player location on destory

--- Assets/1. Code/Movement/Movement.cs
+++ Assets/1. Code/Movement/Movement.cs
@@ -1,4 +1,7 @@
 using System;
+using CleanRoom.Utils;
+using KattenKasteel.FSM;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace CleanRoom.Movement
@@ -6,10 +9,19 @@ namespace CleanRoom.Movement
     [RequireComponent(typeof(Rigidbody2D))]
     public class Movement : MonoBehaviour
     {
+        [Serializable] private struct Vec2
+        {
+            public float x;
+            public float y;
+        }
+        
+        private const string SAVE_SUFFIX = "_player-position.json";
+
         [field: SerializeField] public MovementInput Input { get; private set; }
         [SerializeField] private float movementSpeed;
-        
+
         private new Rigidbody2D rigidbody2D = null;
+        private string filename = string.Empty;
 
         private void Start()
         {
@@ -17,19 +29,36 @@ namespace CleanRoom.Movement
             {
                 throw new Exception("the input has not been assigned");
             }
-
+            
             rigidbody2D = GetComponent<Rigidbody2D>();
+
+            filename = StateMachine.Instance.ActiveState.StateName.ToLower().Replace(' ', '-');
+            filename += SAVE_SUFFIX;
+
+            if (!SaveSystem.Load(filename, out string contents))
+            {
+                return;
+            }
+
+            Vec2 vec2 = JsonConvert.DeserializeObject<Vec2>(contents);
+            transform.position = new Vector3(vec2.x, vec2.y, 0);
+        }
+
+        private void OnDestroy()
+        {
+            Vec2 vec2 = new() { x = rigidbody2D.position.x, y = rigidbody2D.position.y };
+            SaveSystem.Save(filename, JsonConvert.SerializeObject(vec2));
+        }
+
+        public void FixedUpdate()
+        {
+            Move(Input.GetInput());
         }
 
         private void Move(Vector2 inputAxis)
         {
-            Vector2 target = rigidbody2D.position + (inputAxis * (Time.fixedDeltaTime * movementSpeed)); 
+            Vector2 target = rigidbody2D.position + inputAxis * (Time.fixedDeltaTime * movementSpeed);
             rigidbody2D.MovePosition(target);
-        }
-        
-        public void FixedUpdate()
-        {
-            Move(Input.GetInput());
         }
     }
 }
