5218a2be3648885b70177b1b4733bb9a370a77e1
Mathias Bevers <mathiasbevers@protonmail.com>
2026-06-05 13:44:53 +0200

    updated parent state completion check
    - to prevent loading errors it will now used the get state method to
    load the correct state instances.

--- Packages/net.kattenkasteel.finite-state-machine/Runtime/State.cs
+++ Packages/net.kattenkasteel.finite-state-machine/Runtime/State.cs
@@ -10,13 +10,17 @@ namespace KattenKasteel.FSM
         [field: SerializeField] public string StateName { get; private set; }
         [field: SerializeField, Scene] public int SceneIndex { get; private set; }
         [field: SerializeField] public bool IsParent { get; private set; }
-        [field: SerializeField, ShowIf("IsParent")] public State[] Children { get; private set; }
-        
-        private bool isCompleted = false;
+        [field: SerializeField, ShowIf("IsParent")]
+        public State[] Children { get; private set; }
+
         public bool IsCompleted
         {
-            get => IsParent ? Children.All(child => child.IsCompleted) : isCompleted;
+            get => IsParent
+                ? Children.All(c => StateMachine.Instance.GetState(s => string.Equals(s.StateName, c.StateName)))
+                : isCompleted;
             set => isCompleted = value;
         }
+
+        private bool isCompleted = false;
     }
 }
