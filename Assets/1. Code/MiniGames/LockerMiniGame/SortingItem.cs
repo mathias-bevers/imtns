9cc27e31b7087fb9417a7dc3f0834e45be05c143
Mathias Bevers <mathiasbevers@protonmail.com>
2026-06-06 10:43:10 +0200

    added name to the item container
    - added a textmesh pro element to the item container
    - resized item container
    - added name is updated from code

--- Assets/1. Code/MiniGames/LockerMiniGame/SortingItem.cs
+++ Assets/1. Code/MiniGames/LockerMiniGame/SortingItem.cs
@@ -1,5 +1,8 @@
+using System.Globalization;
 using CleanRoom.InventorySystem;
 using CleanRoom.MiniGames.CleanMiniGame;
+using TMPro;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -7,32 +10,35 @@ namespace CleanRoom.MiniGames.LockerMiniGame
 {
     public class SortingItem : DragAndSnap
     {
-        private Image image;
-        public Image Image
-        {
-            get
-            {
-                image ??= GetComponent<Image>();
-                return image;
-            }
-        }
+        [SerializeField] private Image image;
+        [SerializeField] private TextMeshProUGUI text;
+        
         public InventoryItem Data { get; private set; }
 
-        public void Setup(InventoryItem data)
+        public void SetActive(bool isActive)
+        {
+            image.gameObject.SetActive(isActive);
+            text.gameObject.SetActive(isActive);
+        }
+        
+
+        public void DisplayData(InventoryItem data)
         {
             Data = data;
-            Image.sprite = Data.Sprite;
+            image.sprite = Data.Sprite;
+            string capitalizedName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(data.Name);
+            text.SetText(capitalizedName);
         }
 
         public override void OnBeginDrag(PointerEventData eventData)
         {
-            Image.raycastTarget = false;
+            image.raycastTarget = false;
         }
 
         public override void OnEndDrag(PointerEventData eventData)
         {
             base.OnEndDrag(eventData);
-            Image.raycastTarget = true;
+            image.raycastTarget = true;
         }
     }
 }
