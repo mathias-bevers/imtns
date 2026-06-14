cd3933489a9e6b6b9915da914947eb94e3debe92
Mathias Bevers <mathiasbevers@protonmail.com>
2026-06-14 22:38:52 +0200

    made sound settings functional
    - added code for updating the sound mixer
    - create class as an interface for the sliders
    - saved sound settings to file

--- Assets/1. Code/SoundManager.cs
+++ Assets/1. Code/SoundManager.cs
@@ -1,12 +1,22 @@
 using System;
+using CleanRoom.Utils;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
+    private const string FILE_NAME = "sound-settings.json";
+    private const string SFX = "SFXVolume";
+    private const string MAIN = "MainVolume";
+    
     public static SoundManager Instance { get; private set; }
-
+    
+    [Header("Mixer")]
+    [SerializeField] private AudioMixer mixer;
+    
     [Header("Audio Mixer Groups")]
+    [SerializeField] private AudioMixerGroup mainGroup;
     [SerializeField] private AudioMixerGroup sfxGroup;
 
     [Header("Audio Sources")]
@@ -37,11 +47,40 @@ public class SoundManager : MonoBehaviour
         SetupAudioSources();
     }
 
+    private void Start()
+    {
+        SaveSystem.Load(FILE_NAME, out string contents);
+        if (string.IsNullOrEmpty(contents))
+        {
+            return;
+        }
+
+        JObject soundSettings = JObject.Parse(contents);
+        float sfx = soundSettings[SFX]!.ToObject<float>();
+        float main = soundSettings[MAIN]!.ToObject<float>();
+            
+        SetVolumeSFX(sfx);
+        SetVolumeMain(main);
+    }
+
+    private void OnDestroy()
+    {
+        JObject soundSettings = new ();
+        mixer.GetFloat(SFX, out float sfxVolume);
+        soundSettings.Add(SFX, sfxVolume);
+
+        mixer.GetFloat(MAIN, out float mainVolume);
+        soundSettings.Add(MAIN, mainVolume);
+
+        SaveSystem.Save(FILE_NAME, soundSettings.ToString());
+    }
+
     private void SetupAudioSources()
     {
         if (sfxSource != null && sfxGroup != null)
             sfxSource.outputAudioMixerGroup = sfxGroup;
     }
+    
     public void PlaySFX(string soundName)
     {
         SoundEffect effect = Array.Find(soundEffects, x => x.name == soundName);
@@ -54,5 +93,21 @@ public class SoundManager : MonoBehaviour
         {
             Debug.LogWarning($"Sound Effect: {soundName} not found!");
         }
+    }
+
+    public float GetVolume(string key)
+    {
+        mixer.GetFloat(key, out float value);
+        return value;
+    }
+
+    public void SetVolumeSFX(float value)
+    {
+        mixer.SetFloat(SFX, value);
+    }
+
+    public void SetVolumeMain(float value)
+    {
+        mixer.SetFloat(MAIN, value);
     }
 }
