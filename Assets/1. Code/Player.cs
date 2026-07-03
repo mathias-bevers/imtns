36b0dc37ea6b9aeace686b9f0b694acc5ae27d37
Mathias Bevers <mathiasbevers@protonmail.com>
2026-07-03 11:53:39 +0200

    gowned animations are presented after the player completed the dressing game
    - the player behaviour sets a animator boolean to determaine which trees
    it should use
    - the animator boolean is set based on the dressing state completion
    status

--- Assets/1. Code/Player.cs
+++ Assets/1. Code/Player.cs
@@ -1,4 +1,22 @@
+using KattenKasteel.FSM;
+using UnityEngine;
+
 namespace CleanRoom
 {
-    public class Player : Singleton<Player> { }
+    public class Player : Singleton<Player>
+    {
+        private const string DRESSING_GAME_STATE_NAME = "Aankleed Procedure";
+        private static readonly int IS_GOWNED = Animator.StringToHash("IsGowned");
+
+        [SerializeField] private Animator animator;
+
+        private State dressingState;
+
+        public override void Awake()
+        {
+            base.Awake();
+            dressingState = StateMachine.Instance.GetState(s => string.Equals(DRESSING_GAME_STATE_NAME, s.StateName));
+            animator.SetBool(IS_GOWNED, dressingState.IsCompleted); //TODO: uncomment when done testing
+        }
+    }
 }
